a43e4ab53587ad3d1cd90501f71d05d666ec76fa
Pavel Petrov <pavel.g.petrov@gmail.com>
2019-05-18 21:03:34 +0300

    generate matrix

---
+++ WebSite1/Scripts/game.js
@@ -0,0 +1,19 @@
+﻿var Game = (function () {
+    var tiles = [];
+    var init = function (size) {
+        var board = $("#gameBoard");
+        for (var i = 0; i < size; i++) {
+			var row = $("#tileTemplates .row").clone();
+			board.append(row);
+            for (var j = 0; j < size; j++) {
+                var tile = $("#tileTemplates .tile").clone();
+                $("span.tileSpan", tile).html(i * size + j + 1);
+                row.append(tile);
+            }
+        }
+    }
+
+    return {
+        init: init
+    };
+})();
